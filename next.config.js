c7aa5011ca1d1d4e069ce901d691630b26329c41
Carlos Salazar Silva <73184352+Carlangas11@users.noreply.github.com>
2022-08-08 23:13:52 -0400

    first commit for changes about front

--- next.config.js
+++ next.config.js
@@ -1,7 +1,60 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { i18n } = require("./next-i18next.config");
 
-module.exports = nextConfig
+const ContentSecurityPolicy = `default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';font-src 'self';`;
+
+module.exports = {
+  poweredByHeader: false,
+  images: {
+    domains: ["i.imgur.com", "www.google.com", "imgur.com"],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: "graphql-tag/loader",
+    });
+
+    return config;
+  },
+  i18n,
+  webpackDevMiddleware: (config) => config,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  env: {},
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: ContentSecurityPolicy,
+          // },
+          // {
+          //   key: 'Permissions-Policy',
+          //   value: `camera=(); battery=(self); geolocation=*; microphone=()`,
+          // },
+        ],
+      },
+    ];
+  },
+};
